669ed14136603fec06955cac78eb380c3dbcf452
misspelz <pelumiiadetoye@gmail.com>
2024-10-03 22:40:38 +0100

    fix: netlify 4040 page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,21 +1,27 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="bg-secondary p-4 w-full h-full flex justify-between items-center">
-      <a href='/'>
-        <img src='/images/gen.svg' alt="logo" className="w-full h-full" />
-      </a>
+      <Link to="/">
+        <img src="/images/gen.svg" alt="logo" className="w-full h-full" />
+      </Link>
       <div className="flex gap-4">
-        <div className='py-2 px-6 rounded-md bg-gray-400/10 md:flex items-center gap-2 text-white/70 hidden'>
-          <img src='/images/play.png' alt="logo" className="w-6 h-6" />
-          <button className="" disabled>App coming soon.</button>
+        <div className="py-2 px-6 rounded-md bg-gray-400/10 md:flex items-center gap-2 text-white/70 hidden">
+          <img src="/images/play.png" alt="logo" className="w-6 h-6" />
+          <button className="" disabled>
+            App coming soon.
+          </button>
         </div>
-        <a href='/home' className='py-2 px-6 rounded-md bg-gray-400/10 text-white/70 flex items-center gap-2 font-thin cursor-pointer'>
+        <Link
+          to="/home"
+          className="py-2 px-6 rounded-md bg-gray-400/10 text-white/70 flex items-center gap-2 font-thin cursor-pointer"
+        >
           Launch app
-        </a>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
