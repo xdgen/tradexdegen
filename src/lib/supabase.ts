4bd55a450e9364aa8e8c0f6614549451426ae863
Caleb Kalejaiye <155694747+heycalebszn@users.noreply.github.com>
2025-03-08 15:40:34 +0000

    .env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl ="https://pthvfhekcrxzvowbczba.supabase.co"
-const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0aHZmaGVrY3J4enZvd2JjemJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0MjIxOTgsImV4cCI6MjA1Njk5ODE5OH0.vI6l9wq3pypvAbf_Y2lgj1NVO7-7T9FU-T-fSkHnjnY"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
