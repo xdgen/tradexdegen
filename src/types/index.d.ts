c88f8b7feaf3841f959a885236ef35f781ad2fb1
Joseph Ibok <josephibok75@gmail.com>
2025-10-20 17:35:04 +0100

    Integrated check-wallet and register user role apis

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -15,3 +15,29 @@ interface AcademyStats {
   monthlyEnrollments: number;
   retentionRate: number;
 }
+
+interface Token {
+  accessToken: string;
+  refreshToken: string;
+  accessTokenExpiresAt: Date;
+  refreshTokenExpiresAt: Date;
+}
+
+interface User {
+  id: string;
+  wallet: string;
+  role: Role;
+  created_at: Date;
+  updated_at: Date;
+}
+
+interface AuthResponse {
+  token: Token;
+  user: User;
+}
+
+interface CheckUserResponse {
+  status: boolean;
+  message: string;
+  wallet: string;
+}
